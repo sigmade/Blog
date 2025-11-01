fc1897a1c7287499a98dd8a4c819e4b7c1cd8237
Yegor Sychev <yegor@example.com>
2025-11-01 13:23:01 +0500

    fix: update site and base URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://<ВАШ-ЛОГИН-GITHUB>.github.io',
-  base: '/<ИМЯ-ВАШЕГО-РЕПОЗИТОРИЯ>',
+  site: 'https://sigmade.github.io',
+  base: '/blog',
 });
